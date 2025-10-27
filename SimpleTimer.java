c3f7fba6d5f1b000885fbf3c33ceaf01a26c9e53
majkolla <michael.birtman@gmail.com>
2025-10-28 00:11:02 +0100

    asd

--- SimpleTimer.java
+++ SimpleTimer.java
@@ -1,12 +1,9 @@
-import reactor.core.publisher.Flux; 
-import java.time.Duration;
 import java.time.LocalTime;
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 
-public class TimeStreamApp {
+public class SimpleTimer {
     public static void main(String[] args) {
         var scheduler = Executors.newSingleThreadScheduledExecutor();
         scheduler.scheduleAtFixedRate(() -> {System.out.println("time is: " + LocalTime.now());
---
+++ TimeStream.java
@@ -0,0 +1,16 @@
+import java.time.LocalTime;
+import java.time.Duration;
+import reactor.core.publisher.Flux;  
+
+
+public class TimeStream {
+    public static void main(String[] args) throws InterruptedException {
+        // stream that emits one number every second
+        Flux.interval(Duration.ofSeconds(1))
+            .map(tick -> LocalTime.now())
+            .subscribe(time -> System.out.println(time));
+
+
+        Thread.sleep(60_000);
+    }    
+}
